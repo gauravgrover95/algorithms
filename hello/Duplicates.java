4c57d2bdb0c9d8e21031a0815066266d6fb7c38d
Gaurav Grover <gauravgrover95@gmail.com>
2024-10-06 19:41:25 +0530

    added code for duplicates

---
+++ hello/Duplicates.java
@@ -0,0 +1,49 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Duplicates {
+
+    static int[] duplicates(int[] arr) {
+        Map<Integer, Integer> counts = new HashMap<>();
+        for (int i = 0; i < arr.length; ++i) {
+            if (counts.get(arr[i]) != null) {
+                Integer count = counts.get(arr[i]) + 1;
+                counts.put(arr[i], count);
+            }
+            else {
+                counts.put(arr[i], 1);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (entry.getValue() > 1) {
+                list.add(entry.getKey());
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 6, 3, 6, 1 };
+        System.out.println("arr: " + Arrays.toString(arr));
+
+        int[] result = duplicates(arr);
+        System.out.println("result: " + Arrays.toString(result));
+    }
+}
